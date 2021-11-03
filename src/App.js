50a96da702088e12e5eb7a4917c9974410f528df
julian.ramirez <julian09112001@gmail.com>
2021-11-03 18:47:05 -0500

    Primer demo, aplicacion route trucks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, } from 'react';
+import Table from './components/Table';
+import AppService from "./services/AppServices";
+import "./bootstrap.min.css"
 
 function App() {
+
+  const [routes, setRoutes] = useState([])
+
+  useEffect(() => {
+    getRoutes()
+  }, [])
+
+  const getRoutes = () => {
+    AppService.getAllRoutes().then((response) => {
+      const isSuccess = response && response.networkCode === 200;
+      if (isSuccess) {
+        delete response.networkCode;
+        setRoutes(Object.entries(response));
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="main container-fluid" style={{ marginBottom: "30px", background: "linear-gradient(#82888C ,#8b949a, #FFFFFF)", padding: "50px" }}>
+        <center>
+          <img style={{ height: "96" }} src="https://assertivemoving.com/wp-content/uploads/2020/09/Group-124.png" alt="Company logo" />
+        </center>
+      </div>
+      <div className="main container-fluid " style={{ width: "80%" }}>
+        {routes?.map((route) => (
+          <Table data={route[1]} title={route[0]} />
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
